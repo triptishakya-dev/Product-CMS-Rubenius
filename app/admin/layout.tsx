5c3d4cabfd208a41ca37fb8962abf0b94b3c4b7d
triptishakya-dev <triptishakya.2001@gmail.com>
2026-03-22 03:00:23 +0530

    feat: Implement initial application layout and routing structure, including a dedicated admin section.

---
+++ app/admin/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex-1">
+      {children}
+    </div>
+  );
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>ADMIN PAGE</div>
+  )
+}
+
+export default page
